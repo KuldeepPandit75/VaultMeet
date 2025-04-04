bbb8488c1f1c73207aadce7367d0af77cad72520
KuldeepPandit75 <kuldeepk11139@gmail.com>
2025-04-04 13:22:52 +0530

    type ts fix 6

--- frontend/src/global.t.ts
+++
@@ -1,5 +0,0 @@
-declare module './components/Canvas/PhaserGame.js';
-declare module './features/socket.js';
-declare module './app/store.js';
-declare module './features/slice.js';
-declare module './components/Canvas/Scenes/Lobby.js';
