13c9c536c52f0186bf2a3595b394d4f86dd2f092
KuldeepPandit75 <kuldeepk11139@gmail.com>
2025-04-04 13:31:58 +0530

    type ts fix 8

--- frontend/src/components/Canvas/Scenes/Lobby.d.ts
+++ frontend/src/components/Canvas/Scenes/Lobby.d.ts
@@ -1,1 +1,24 @@
-declare module './Lobby.js';
+import Phaser from 'phaser';
+import { Socket } from 'socket.io-client';
+
+declare class Lobby extends Phaser.Scene {
+    socket: Socket | null;
+    otherPlayers: { [id: string]: Phaser.Physics.Arcade.Sprite };
+    player: Phaser.Physics.Arcade.Sprite;
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+    distBwUnP: number;
+
+    constructor();
+
+    preload(): void;
+
+    create(): void;
+
+    update(): void;
+
+    initializeSocket(): void;
+
+    shutdown(): void;
+}
+
+export default Lobby;
