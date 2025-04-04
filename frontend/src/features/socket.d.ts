1976abdffe785167cdfa5638f3909982f86a42de
KuldeepPandit75 <kuldeepk11139@gmail.com>
2025-04-04 13:16:24 +0530

    type ts fix 5

--- frontend/src/features/socket.d.ts
+++ frontend/src/features/socket.d.ts
@@ -1,1 +1,7 @@
-declare module './socket.js';
+// socket.d.ts
+declare module './socket' {
+    import type { Socket } from 'socket.io-client';
+  
+    export function getSocket(): Socket;
+  }
+  
