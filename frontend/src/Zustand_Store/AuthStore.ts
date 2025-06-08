08e325b0b1de8bb62e37a3ee5dbcb04bdee601ea
KuldeepPandit75 <kuldeepk11139@gmail.com>
2025-06-09 00:05:11 +0530

    saving cookie on frontend 2

--- frontend/src/Zustand_Store/AuthStore.ts
+++ frontend/src/Zustand_Store/AuthStore.ts
@@ -1,7 +1,6 @@
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
 import axios, { AxiosError } from 'axios';
-import { cookies } from 'next/headers';
 
 export enum Role {
   Admin = 'admin',
