92a964dcf9f529792e3e022703704ae780c52782
KuldeepPandit75 <kuldeepk11139@gmail.com>
2025-07-19 21:28:48 +0530

    route nav issue fix 4

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,38 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['images.unsplash.com','res.cloudinary.com','lh3.googleusercontent.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/coding-space/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/event-space/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/me',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+  
