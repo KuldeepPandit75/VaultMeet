fb6edd185f8c7bb2ce5df7d4d1e6279f06ec3eef
KuldeepPandit75 <kuldeepk11139@gmail.com>
2025-08-17 16:45:29 +0530

    remove icon fixed

--- frontend/src/components/Game/Modals/ModerationModal.tsx
+++ frontend/src/components/Game/Modals/ModerationModal.tsx
@@ -292,8 +292,8 @@ const ModerationModal: React.FC<ModerationModalProps> = ({
               }`}
             >
               <div className="flex items-center justify-center space-x-2">
-                <svg className="w-5 h-5 text-yellow-500" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" clipRule="evenodd" />
+                <svg className="w-5 h-5 text-yellow-500" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
                 <span>Remove Participants</span>
               </div>
